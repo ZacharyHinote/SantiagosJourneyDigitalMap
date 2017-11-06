a12507bb3f0549fb63aff66e86c79270c91e07ee
Zac Hinote <zacfirebender@gmail.com>
2017-11-06 07:46:53 -0600

    Update general.js

--- general.js
+++ general.js
@@ -23,7 +23,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
-  if (n == 0) {
     document.getElementById("paragraph").innerHTML = n.toString();
-  }
 }
