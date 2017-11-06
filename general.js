defcc001ce42c792840977617c256a8ed40696ea
Zac Hinote <zacfirebender@gmail.com>
2017-11-06 07:46:21 -0600

    Update general.js

--- general.js
+++ general.js
@@ -24,6 +24,6 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
   if (n == 0) {
-    document.getElementById("paragraph").innerHTML = "Spain";
+    document.getElementById("paragraph").innerHTML = n.toString();
   }
 }
