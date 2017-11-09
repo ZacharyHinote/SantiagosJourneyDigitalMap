f94bedeece8fd4754e4b53960dba9dbbefc0fcba
Zac Hinote <zacfirebender@gmail.com>
2017-11-09 07:56:53 -0600

    Update general.js

--- general.js
+++ general.js
@@ -26,23 +26,27 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
   if (n == 1) {
-    document.getElementById("hOne").innerHTML = "Spain, Andalusia";
-    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Andalusia\">Spain, Andalusia</a>";
+    document.getElementById("hOne").innerHTML = "Andalusia, Spain in Europe";
+    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Andalusia\">Andalusia, Spain</a>";
   }
   if (n == 2) {
-    document.getElementById("hOne").innerHTML = "Spain, Tarifa";
-    
+    document.getElementById("hOne").innerHTML = "Tarifa, Spain in Europe";
+    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Tarifa\">Tarifa, Spain</a>";
   }
   if (n == 3) {
-    document.getElementById("hOne").innerHTML = "Africa, Tangier";
+    document.getElementById("hOne").innerHTML = "Tangier, Morocco in Africa";
+    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Tangier\">Tangier, Morocco</a>";
   }
   if (n == 4) {
     document.getElementById("hOne").innerHTML = "Sahara Desert, Egypt";
+    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Sahara\">Sahara Desert, Egypt</a>";
   }
   if (n == 5) {
     document.getElementById("hOne").innerHTML = "The Oasis in the Sahara Desert";
+    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Oasis\">Oasis</a>";
   }
   if (n == 6) {
     document.getElementById("hOne").innerHTML = "The Egyptian Pyramids";
+    document.getElementById("Para").innerHTML = "<a href=\"https://en.wikipedia.org/wiki/Egyptian_pyramids\">Egyptian Pyramids</a>";
   }
 }
